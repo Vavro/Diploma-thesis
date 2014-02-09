9e62567d39a685eabb7823af3644a7e381991163
Petr Vavro <BlackSun.Petr@gmail.com>
2014-02-09 14:27:52 +0100

    Split objects to method

--- Src/DragqnLD/DragqnLD.Core/Implementations/ExpandedJsonLDDataFormatter.cs
+++ Src/DragqnLD/DragqnLD.Core/Implementations/ExpandedJsonLDDataFormatter.cs
@@ -26,12 +26,43 @@ namespace DragqnLD.Core.Implementations
             var objects = new Dictionary<string, JObject>();
             JObject rootObject = null;
 
+            SplitObjectsFromGraph(rootObjectId, graphObjects, ref rootObject, objects);
+
+            foreach (var property in rootObject.Properties())
+            {
+                if (property.Name[0] == '@') //skip all JSON-LD keyword properties - @id, @type, @context etc.
+                    continue;
+
+                var asJObject = property.Value as JObject; // go throught nested object and replace ids
+                if (asJObject != null)
+                {
+                    
+                }
+                var asJArray = property.Value as JArray; //go throught and detect whether it contains more objects, or ids
+                if (asJArray != null)
+                {
+                    
+                }
+                var asValue = property.Value as JValue; // replace if it is an id? - not presented in sample data
+                if (asValue != null)
+                {
+                    
+                }
+            }
+        }
+
+        private static void SplitObjectsFromGraph(string rootObjectId, JEnumerable<JToken> graphObjects, ref JObject rootObject,
+            Dictionary<string, JObject> objects)
+        {
             foreach (var graphObject in graphObjects)
             {
-                var id = (string)graphObject["@id"];
+                var id = (string) graphObject["@id"];
                 var typed = graphObject as JObject;
-                if (id == null || typed == null) 
-                        throw new NotSupportedException(String.Format("The collection of objects in the graph property, didn't contain only objects with ids - invalid json: {0}", graphObject.ToString()));
+                if (id == null || typed == null)
+                    throw new NotSupportedException(
+                        String.Format(
+                            "The collection of objects in the graph property, didn't contain only objects with ids - invalid json: {0}",
+                            graphObject.ToString()));
 
                 if (id == rootObjectId)
                 {
@@ -44,9 +75,9 @@ namespace DragqnLD.Core.Implementations
             }
 
             if (rootObject == null)
-                throw new NotSupportedException(String.Format("The collection of objects in the graph property, didn't contain the object with id: {0}", rootObjectId));
-
-
+                throw new NotSupportedException(
+                    String.Format("The collection of objects in the graph property, didn't contain the object with id: {0}",
+                        rootObjectId));
         }
     }
 }
