e6b7d52f27741ff4944108377417cd5edfb12ca2
Petr Vavro <BlackSun.Petr@gmail.com>
2014-02-26 18:04:19 +0100

    Refactor perf test to Theory for exact property matches

--- Src/DragqnLD/Tests/DragqnLD.Core.UnitTests/RavenDtaaStoreQueryPerformanceTests.cs
+++ Src/DragqnLD/Tests/DragqnLD.Core.UnitTests/RavenDtaaStoreQueryPerformanceTests.cs
@@ -10,6 +10,7 @@ using DragqnLD.Core.Implementations.Utils;
 using Raven.Json.Linq;
 using Raven.Tests.Helpers;
 using Xunit;
+using Xunit.Extensions;
 
 namespace DragqnLD.Core.UnitTests
 {
@@ -22,54 +23,36 @@ namespace DragqnLD.Core.UnitTests
             _formatter = new ExpandedJsonLDDataFormatter();
         }
 
-        [Fact]
-        public async Task QueryRootExactDescriptionIngredientProperty()
+        [Theory]
+        [InlineData("QueryDefinitions/1", 
+            TestDataFolders.Ingredients, 
+            @"http://linked.opendata.cz/resource/drug-encyclopedia/ingredient/",
+            @"http://linked.opendata.cz/ontology/drug-encyclopedia/description,@value",
+            @"""Analgesic antipyretic derivative of acetanilide. It has weak anti-inflammatory properties and is used as a common analgesic, but may cause liver, blood cell, and kidney damage.     """)]
+        [InlineData("QueryDefinitions/2",
+            TestDataFolders.MedicinalProducts,
+            @"http://linked.opendata.cz/resource/sukl/medicinal-product/",
+            @"http://linked.opendata.cz/ontology/drug-encyclopedia/title,@value",
+            @"""ABILIFY 7,5 MG/ML""")]
+        public async Task QueryExactPropertyValueProperty(string queryId, string inputFolder, string idPrefix, string searchedProperty, string searchedValue)
         {
-            var queryId = "QueryDefinitions/1";
-            var inputFolder = TestDataFolders.Ingredients;
-            var idPrefix = @"http://linked.opendata.cz/resource/drug-encyclopedia/ingredient/";
             var documents = ConstructResultsForFolder(inputFolder, queryId, idPrefix);
-
-            var searchedDescritpion =
-                @"""Analgesic antipyretic derivative of acetanilide. It has weak anti-inflammatory properties and is used as a common analgesic, but may cause liver, blood cell, and kidney damage.     """;
-            var searchedId = @"http://linked.opendata.cz/resource/drug-encyclopedia/ingredient/M0000115";
 
             await _ravenDataStore.BulkStoreDocuments(documents);
 
             _documentStore.Conventions.ShouldCacheRequest = should => false;
-            TestUtilities.Profile("Query exact Description", 100, () =>
-            {
-                var task =  _ravenDataStore.QueryDocumentProperties(queryId,
-                        @"http://linked.opendata.cz/ontology/drug-encyclopedia/description,@value".AsCondition(searchedDescritpion));
-                task.Wait();
-            });
+            TestUtilities.Profile(
+                String.Format("Query exact property value \n in {0} \n property {1} \n value {2}", idPrefix, searchedProperty, searchedValue), 
+                100, 
+                () =>
+                    {
+                        var task =  _ravenDataStore.QueryDocumentProperties(queryId,
+                                searchedProperty.AsCondition(searchedValue));
+                        task.Wait();
+                    });
             RavenTestBase.WaitForUserToContinueTheTest(_documentStore);
         }
-
-        [Fact]
-        public async Task QueryRootExactDescriptionMedicianalProductProperty()
-        {
-            var queryId = "QueryDefinitions/1";
-            var inputFolder = TestDataFolders.MedicinalProducts;
-            var idPrefix = @"http://linked.opendata.cz/resource/drug-encyclopedia/ingredient/";
-            var documents = ConstructResultsForFolder(inputFolder, queryId, idPrefix);
-
-            var searchedDescritpion =
-                @"""Analgesic antipyretic derivative of acetanilide. It has weak anti-inflammatory properties and is used as a common analgesic, but may cause liver, blood cell, and kidney damage.     """;
-            var searchedId = @"http://linked.opendata.cz/resource/drug-encyclopedia/ingredient/M0000115";
-
-            await _ravenDataStore.BulkStoreDocuments(documents);
-
-            _documentStore.Conventions.ShouldCacheRequest = should => false;
-            TestUtilities.Profile("Query exact Description", 100, () =>
-            {
-                var task = _ravenDataStore.QueryDocumentProperties(queryId,
-                        @"http://linked.opendata.cz/ontology/drug-encyclopedia/description,@value".AsCondition(searchedDescritpion));
-                task.Wait();
-            });
-            RavenTestBase.WaitForUserToContinueTheTest(_documentStore);
-        }
-
+        
         private IEnumerable<ConstructResult> ConstructResultsForFolder(string inputFolder, string queryId, string idPrefix)
         {
             var inputDirectoryInfo = new DirectoryInfo(inputFolder);
