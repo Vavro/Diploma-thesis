5b31c85eefc396f438c9ccc4ea6c1f90b0dcdb82
Petr Vavro <BlackSun.Petr@gmail.com>
2014-04-06 20:49:19 +0200

    Add project for  management website - knockout, durandal, bootstrap

---
+++ Src/DragqnLD/DragqnLD.Web.Html5/App/main.js
@@ -0,0 +1,34 @@
+﻿requirejs.config({
+    paths: {
+        text: '../Scripts/text',
+        durandal: '../Scripts/durandal',
+        plugins: '../Scripts/durandal/plugins',
+        transitions: '../Scripts/durandal/transitions',
+        knockout: '../Scripts/knockout-3.1.0',
+        jquery: '../Scripts/jquery-1.9.1'
+    }
+});
+
+define(function (require) {
+    var system = require('durandal/system'),
+        app = require('durandal/app'),
+        viewLocator = require('durandal/viewLocator');
+
+    system.debug(true);
+
+    app.title = 'DragqnLD Management';
+
+    app.configurePlugins({
+        router: true,
+        dialog: true
+    });
+
+    app.start().then(function () {
+        //Replace 'viewmodels' in the moduleId with 'views' to locate the view.
+        //Look for partial views in a 'views' folder in the root.
+        viewLocator.useConvention();
+
+        //Show the app by setting the root view model for our application with a transition.
+        app.setRoot('viewmodels/shell');
+    });
+});
