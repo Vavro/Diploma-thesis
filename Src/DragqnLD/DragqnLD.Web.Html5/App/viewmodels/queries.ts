2c1f51901710570a884283bcecddf7d25de22e54
Petr Vavro <BlackSun.Petr@gmail.com>
2014-04-30 22:11:54 +0200

    Workaround for showing kogrid with right measurements (width, height)

--- Src/DragqnLD/DragqnLD.Web.Html5/App/viewmodels/queries.ts
+++ Src/DragqnLD/DragqnLD.Web.Html5/App/viewmodels/queries.ts
@@ -1,11 +1,18 @@
 ﻿class queries {
     queries = ko.observableArray();
+    isAttached = ko.observable(false);
     activate(): void {
         var self = this;
 
         $.getJSON("http://localhost:2429/api/queries", (data, textStatus, jqXHR) => {
             self.queries(data);
         });
+    }
+
+    compositionComplete(): void {
+        console.log('queries view attached');
+        this.isAttached(true);
+        $(window).trigger('resize');
     }
 }
 
