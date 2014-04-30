479d5b6411a4c838a4249d2c322a3d65e38894b6
Petr Vavro <BlackSun.Petr@gmail.com>
2014-04-30 23:18:36 +0200

    CORS solution

--- Src/DragqnLD/DragqnLD.WebApi/App_Start/WebApiConfig.cs
+++ Src/DragqnLD/DragqnLD.WebApi/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace DragqnLD.WebApi
 {
@@ -19,6 +20,8 @@ namespace DragqnLD.WebApi
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            config.EnableCors(new EnableCorsAttribute("*","*","*"));
         }
     }
 }
