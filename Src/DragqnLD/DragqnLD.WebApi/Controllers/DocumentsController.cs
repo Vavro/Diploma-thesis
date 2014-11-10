2b1ad00cd540fd9de8f2156845af6282ac02a67b
Petr Vavro <BlackSun.Petr@gmail.com>
2014-11-10 03:36:56 +0100

    Document controller for getting docs

---
+++ Src/DragqnLD/DragqnLD.WebApi/Controllers/DocumentsController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using DragqnLD.Core.Abstraction;
+using DragqnLD.Core.Abstraction.Data;
+using DragqnLD.Core.Implementations;
+
+namespace DragqnLD.WebApi.Controllers
+{
+    public class DocumentsController : BaseApiController
+    {
+        private readonly IDataStore _dataStore;
+
+        public DocumentsController()
+        {
+            _dataStore = new RavenDataStore(Store, new DocumentPropertyEscaper());
+        }
+
+        [HttpGet]
+        [Route("api/query/{definitionId}/documents")]
+        public async Task<HttpResponseMessage> Get()
+        {
+            return Request.CreateResponse(HttpStatusCode.NotImplemented);
+        }
+
+        [HttpGet]
+        [Route("api/query/{definitionId}/document")]
+        public async Task<HttpResponseMessage> Get(string documentId)
+        {
+            var document = await _dataStore.GetDocument(DefinitionId, new Uri(documentId));
+
+            //todo: get rid of Content property write raw json to response
+            return Request.CreateResponse(HttpStatusCode.OK, document);
+        }
+    }
+}
