cf094fd9904513aa905ecd136fcf2a8120d397b6
Petr Vavro <BlackSun.Petr@gmail.com>
2014-11-25 01:12:04 +0100

    breadcrumbs on viewDocument

--- Src/DragqnLD/DragqnLD.Web.Html5/App/viewmodels/viewDocument.ts
+++ Src/DragqnLD/DragqnLD.Web.Html5/App/viewmodels/viewDocument.ts
@@ -1,13 +1,26 @@
 ﻿import viewModelBase = require("viewmodels/viewModelBase");
 import getDocumentCommand = require("commands/getDocumentCommand");
+import router = require("plugins/router");
 
 class viewDocument extends viewModelBase {
+    public documentId = ko.observable<string>();
+    public definitionId = ko.observable<String>();
     public document = ko.observable<any>();
+    public navigateToQueriesUrl: KnockoutComputed<string>;
+    public navigateToDefinitionUrl : KnockoutComputed<string>;
+
+    constructor() {
+        super();
+        this.navigateToQueriesUrl = ko.computed(() => "#queries");
+        this.navigateToDefinitionUrl = ko.computed(() => "#viewQuery?id=" + this.definitionId());
+    }
 
     public canActivate(args: any) : JQueryDeferred<{}> {
         var can = $.Deferred();
 
         if (args && args.documentId && args.definitionId) {
+            this.documentId(args.documentId);
+            this.definitionId(args.definitionId);
             var command = new getDocumentCommand(args.definitionId, args.documentId);
             command.execute().done(result => {
                 this.document(this.stringify(result));
