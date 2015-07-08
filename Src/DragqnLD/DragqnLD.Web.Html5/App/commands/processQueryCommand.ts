544a2ba90972ac524f2a8cb5ba49a6fac3631874
Petr Vavro <BlackSun.Petr@gmail.com>
2015-07-08 17:19:37 +0200

    report error on task process call

--- Src/DragqnLD/DragqnLD.Web.Html5/App/commands/processQueryCommand.ts
+++ Src/DragqnLD/DragqnLD.Web.Html5/App/commands/processQueryCommand.ts
@@ -13,8 +13,9 @@ class processQueryCommand extends commandBase {
         var response = $.Deferred();
         var url = "/" + encodeURIComponent(this.id) + "/process";
         var queryResult = this.query<any>(url, null, null);
-        queryResult.fail(
-            xhr => response.reject(xhr));
+        queryResult.fail((r: JQueryXHR): void => {
+            this.notifyError("Failed to launch processing of qurey " + this.id, r.responseText, r.statusText);
+        });
         queryResult.done((result: any, status: any, xhr: JQueryXHR): void => {
             response.resolve(result);
         });
